943270dc60c3e723d6a46c5742a6a193098e00d8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-28 15:12:56 +0800

    export gene dblinks

--- src/exportServlet.php
+++ src/exportServlet.php
@@ -24,4 +24,36 @@ class App {
             "family" => $q
         ]);
     }
+
+    /**
+     * Export database id cross reference
+     * 
+     * @param string $src the external database name
+    */
+    public function gene_dblink($src, $page = 1, $page_size = 1000) {
+        $tbl = new Table("dblinks");
+        $term = (new Table("vocabulary"))->where(["hashcode" => registry_key($src)])->find();
+
+        if (Utils::isDbNull($term)) {
+            controller::error("The given data source could not be found in registry!");
+        }
+
+        $start = ($page - 1) * $page_size;
+        $q = $tbl->left_join("molecules")
+        ->on(["molecules" => "id", "dblinks" => "entity_id"])
+        ->where([
+            "entity_type" => 1,
+            "db_src" => $term["id"] 
+        ])
+        ->limit($start, $page_size)
+        ->select(["xref_id", "`molecule_id` as gene_id", "name", "`molecules`.`id` as biocad_id" ])
+        ;
+
+        controller::success([            
+            "count" => count($q),
+            "start" => $start,
+            "has_next" => count($q) == $page_size,
+            "query" => $q
+        ], $tbl->getLastMySql());
+    }
 }
