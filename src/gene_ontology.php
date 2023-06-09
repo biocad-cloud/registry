a6ad798b889f5244c41435388384e9f38aaed31b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-10 00:19:31 +0800

    fix for create gene ontology dag graph

--- src/gene_ontology.php
+++ src/gene_ontology.php
@@ -6,16 +6,46 @@ class App {
 
     /**
      * add a new gene ontology term into registry
+     * 
+     * @method POST
     */
     public function add($id, $name, $namespace, $is_a, $xref, $def, $synonym) {
         $go = new Table("gene_ontology");
-        $go->add([
-            "id" => $id,
-            "name" => $name,
-            "namespace" => $namespace,
-            "def" => $def
-        ]);
+        $dag = new Table("go_dag");
+        $id = strip_postVal($id);
+        $find = $go->where(["id" => $id])->find();
 
-        
+        if (Utils::isDbNull($find)) {
+            $go->add([
+                "id" => $id,
+                "name" => strip_postVal($name),
+                "namespace" => strip_postVal($namespace),
+                "def" => strip_postVal($def)
+            ]);
+        }
+
+        if (is_string($is_a)) {
+            $is_a = json_decode($is_a, true);
+        }
+
+        if ($is_a["count"] > 0) {
+            if ($is_a["count"] == 1) {             
+                $dag->add([
+                    "term_id" => $id,
+                    "go_term" => $is_a["data"],
+                    "relationship" => 1
+                ]);
+            } else {
+                foreach($is_a["data"] as $term) {
+                    $dag->add([
+                        "term_id" => $id,
+                        "go_term" => $term,
+                        "relationship" => 1
+                    ]);
+                }
+            }
+        }
+
+        controller::success(0);
     }
 }
