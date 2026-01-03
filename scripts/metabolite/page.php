cb82b39f2b8cac73d0c569d5c25a1240df43b266
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-03 14:27:06 +0800

    adjust of the page display

--- scripts/metabolite/page.php
+++ scripts/metabolite/page.php
@@ -3,24 +3,9 @@
 class metabolite_page {
     
     public static function page_data($id) {
-        $id = Regex::Match($id, "\d+");
-        $page = new Table(["cad_registry"=>"metabolites"]);
-        $page = $page
-            ->left_join("struct_data")
-            ->on(["metabolites"=>"id","struct_data"=>"metabolite_id"])
-            ->where(["`metabolites`.`id`"=>$id])
-            ->find(["metabolites.*", "smiles","fingerprint","pdb_data"])
-            ;
+        $page = self::page_json($id);
         $page["title"] = $page["name"];
-        $page["id"] = "BioCAD" . str_pad($id,11,'0', STR_PAD_LEFT);
-        $synonyms = new Table(["cad_registry"=>"synonym"]);
-        $synonyms = $synonyms
-            ->where(["type" => ENTITY_METABOLITE, "obj_id"=>$id])
-            ->group_by("hashcode")
-            ->order_by("COUNT(*)", true)
-            ->limit(6)
-            ->select(["hashcode","MIN(synonym) AS synonym","COUNT(*) AS refers","MIN(lang) AS language","GROUP_CONCAT(DISTINCT db_source) AS db_source"])
-            ;
+        $langs = [];
         $synonyms = array_map(function($name) {
             $dbs = explode(",", $name["db_source"]);
             $dbs = enum_to_dbnames($dbs);
@@ -30,14 +15,44 @@ class metabolite_page {
                 "lang"=>$name["language"],
                 "db_source"=> Strings::Join($dbs,",")
             ];
-        }, $synonyms);
-        $langs = [];
+        }, $page["synonyms"]);        
 
         foreach ($synonyms as $item) {
             $langs[$item['lang']][] = "<span title='{$item["db_source"]}'>{$item["name"]}</span>";
         }
 
-        $page["synonyms"] = $langs;
+        $synonyms = "";
+
+        foreach($langs as $lang => $list) {
+            $list = Strings::Join($list,"; ");
+            $synonyms = $synonyms . "<h5>Synonyms [$lang]</h5>
+            <p>$list</p>";
+        }
+
+        $page["synonyms"] = $synonyms;
+
+        return $page;
+    }
+
+    public static function page_json($id) {
+        $id = Regex::Match($id, "\d+");
+        $page = new Table(["cad_registry"=>"metabolites"]);
+        $page = $page
+            ->left_join("struct_data")
+            ->on(["metabolites"=>"id","struct_data"=>"metabolite_id"])
+            ->where(["`metabolites`.`id`"=>$id])
+            ->find(["metabolites.*", "smiles","fingerprint","pdb_data"])
+            ;        
+        $page["id"] = "BioCAD" . str_pad($id,11,'0', STR_PAD_LEFT);
+        $synonyms = new Table(["cad_registry"=>"synonym"]);
+        $synonyms = $synonyms
+            ->where(["type" => ENTITY_METABOLITE, "obj_id"=>$id])
+            ->group_by("hashcode")
+            ->order_by("COUNT(*)", true)
+            ->limit(6)
+            ->select(["hashcode","MIN(synonym) AS synonym","COUNT(*) AS refers","MIN(lang) AS language","GROUP_CONCAT(DISTINCT db_source) AS db_source"])
+            ;
+        $page["synonyms"] = $synonyms;
 
         return $page;
     }
