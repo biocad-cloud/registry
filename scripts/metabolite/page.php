65d910c2d0bb0131e207dd578981f93a21a7906a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-03 12:31:53 +0800

    Update page.php

--- scripts/metabolite/page.php
+++ scripts/metabolite/page.php
@@ -5,7 +5,12 @@ class metabolite_page {
     public static function page_data($id) {
         $id = Regex::Match($id, "\d+");
         $page = new Table(["cad_registry"=>"metabolites"]);
-        $page = $page->where(["id"=>$id])->find();
+        $page = $page
+            ->left_join("struct_data")
+            ->on(["metabolites"=>"id","struct_data"=>"metabolite_id"])
+            ->where(["`metabolites`.`id`"=>$id])
+            ->find(["metabolites.*", "smiles","fingerprint","pdb_data"])
+            ;
         $page["title"] = $page["name"];
         $page["id"] = "BioCAD" . str_pad($id,11,'0', STR_PAD_LEFT);
         return $page;
