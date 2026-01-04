993abdd8d783157cfa2eefaa77838e70a093a5f5
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-04 23:28:56 +0800

    Update registry.php

--- .etc/registry.php
+++ .etc/registry.php
@@ -1,7 +1,7 @@
 <?php
 
 # constant from cad_registry.vocabulary table
-define("ENTITY_METABOLITE", 10);
+define("ENTITY_METABOLITE", 11);
 
 define("DB_CAS", 1);
 define("DB_PUBCHEM", 2);
@@ -13,8 +13,8 @@ define("DB_BIOCYC", 7);
 define("DB_MESH", 8);
 define("DB_WIKIPEDIA", 9);
 define("DB_REFMET", 11);
-define("DB_DRUGBANK", 12);
-define("DB_CLASSYFIRE", 13);
+define("DB_DRUGBANK", 10);
+define("DB_CLASSYFIRE", 12);
 
 function enum_to_dbnames($ids) {
     return array_map(function($id) {
