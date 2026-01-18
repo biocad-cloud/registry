98b31db2c968353a46d9b57db727ac0b85c3ccae
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-19 05:47:15 +0800

    tweaks of the page display

--- scripts/protein/fasta.php
+++ scripts/protein/fasta.php
@@ -10,7 +10,30 @@ class prot_fasta {
             ->find(["`protein_data`.*",
                 "`ncbi_taxonomy`.name as taxname"
             ]);
+        $ec = (new Table(["cad_registry"=>"db_xrefs"]))
+            ->where(["type" => FASTA_PROTEIN,"db_name"=>EC_NUMBER,"obj_id"=>$id])
+            ->project("db_xref")
+            ;
+        $rxns = [];
+
+        if (count($ec) > 0) {
+            $rxns = (new Table(["cad_registry"=>"db_xrefs"]))->where(["type"=>ENTITY_REACTION,"db_name"=>EC_NUMBER,"db_xref"=>in($ec)])->project("obj_id");
+
+            if (count($rxns) > 0) {
+                $rxns = (new Table(["cad_registry"=>"reaction"]))
+                    ->left_join("vocabulary")
+                    ->on(["vocabulary"=>"id","reaction"=>"db_source"])
+                    ->where(["`reaction`.id"=>in($rxns)])
+                    ->select(["reaction.id","name","reaction.note","term as db_source","db_xref"])
+                    ;
+            }
+        }
+
+        $seq["reaction"] = $rxns;
         $seq["title"] = $seq["name"] . " - " . $seq["taxname"];
+        $seq["ec_numbers"] = Strings::Join(array_map(function($ec) {
+            return "<a href='/proteins/?ec={$ec}'>$ec</a>";
+        }, $ec), " / ");
 
         return $seq;
     }
