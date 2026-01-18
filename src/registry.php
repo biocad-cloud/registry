95a6bd479f4df216f44f058ae94300d149f4e206
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-19 04:52:01 +0800

    Update registry.php

--- src/registry.php
+++ src/registry.php
@@ -4,8 +4,19 @@ include __DIR__ . "/../.etc/bootstrap.php";
 
 class App {
 
+    /**
+     * @uses api
+    */
     public function metabolite($id) {
         include APP_PATH . "/scripts/metabolite/page.php";
+
+        $id = metabolite_page::resolve_id($id);
+        $main_id = metabolite_page::resolve_main($id);
+
+        if ($main_id > 0) {
+            $id = $main_id;
+        }
+
         controller::success(metabolite_page::page_data($id));
     }
 }
