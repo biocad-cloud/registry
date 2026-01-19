94357be3864f6cd71ac46e2b23999847a1425276
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-19 17:12:00 +0800

    make code style improvements

--- scripts/motifs/list.php
+++ scripts/motifs/list.php
@@ -4,7 +4,12 @@ class motif_list {
 
     public static function get_list($page,$page_size=15) {
         $offset = ($page-1) * $page_size;
-        $list = (new Table(["cad_registry"=>"motif"]))->group_by("family")->order_by("family")->limit($offset,$page_size)->select(["family","count(*) as size"]);
+        $list = (new Table(["cad_registry"=>"motif"]))
+            ->group_by("family")
+            ->order_by("family")
+            ->limit($offset,$page_size)
+            ->select(["family","count(*) as size"])
+            ;
 
         for($i=0;$i < count($list);$i++) {
             $list[$i]["family_id"] = urlencode($list[$i]["family"]);
--- scripts/motifs/page.php
+++ scripts/motifs/page.php
@@ -5,7 +5,10 @@ class motif_data {
     public static function getdata($id) {
         $motif = (new Table(["cad_registry"=>"motif"]))->where(["id"=>$id])->find();
         $motif["title"] = $motif["name"];
-        $motif["site"] = (new Table(["cad_registry"=>"nucleotide_data"]))->where(["is_motif"=>1, "model_id"=>$id])->select(["name","sequence","organism_source AS ncbi_taxid"]);
+        $motif["site"] = (new Table(["cad_registry"=>"nucleotide_data"]))
+            ->where(["is_motif"=>1, "model_id"=>$id])
+            ->select(["name","sequence","organism_source AS ncbi_taxid"])
+            ;
         $tax_id = array_column($motif["site"], "ncbi_taxid");
         $motif["tax"] = (new Table(["cad_registry"=>"ncbi_taxonomy"]))->where(["id"=>in($tax_id)])->select();
         $motif["site"] = json_encode($motif["site"]);      
