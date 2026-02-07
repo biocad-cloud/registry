5a68ba60bd1573b14e66bdbd6d09244bbbe8e3a1
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-08 02:18:00 +0800

    add IconCaptcha-4.0.5.manual-install package

---
+++ framework/IconCaptcha/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fabianwennink/iconcaptcha',
+        'pretty_version' => '4.x-dev',
+        'version' => '4.9999999.9999999.9999999-dev',
+        'reference' => '18b0d137993cf1b88817876b4edd89470024624b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'fabianwennink/iconcaptcha' => array(
+            'pretty_version' => '4.x-dev',
+            'version' => '4.9999999.9999999.9999999-dev',
+            'reference' => '18b0d137993cf1b88817876b4edd89470024624b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
