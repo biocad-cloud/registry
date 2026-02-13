142c096b035bb80ab372c94c34015ca9744051f7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-13 17:36:36 +0800

    make improvements of the data page

--- scripts/compartments/page.php
+++ scripts/compartments/page.php
@@ -18,8 +18,16 @@ class compartment_location {
             ->limit(100)
             ->select(["`metabolites`.id","name","formula","ROUND(exact_mass, 4) AS exact_mass"])
             ;
+        $proteins = (new Table(["cad_registry"=>"subcellular_location"]))
+            ->left_join("protein_data")
+            ->on(["protein_data"=>"id","subcellular_location"=>"protein_id"])
+            ->where(["location_id"=>$loc["id"]])
+            ->limit(100)
+            ->select("protein_data.*")
+            ;
 
         $loc["metabolite"] = $metabolites;
+        $loc["protein"] = $proteins;
         $loc["title"] = $loc["fullname"];
 
         return $loc;
