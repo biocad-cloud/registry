ed8b7c323f7c223ef16a8df9413d4e173c3ed554
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-19 02:12:28 +0800

    show kientics law data

--- scripts/enzyme/page.php
+++ scripts/enzyme/page.php
@@ -3,8 +3,16 @@
 class enzyme_data {
 
     public static function data($ec) {
+        $laws = (new Table(["cad_registry"=>"kinetics_law"]))
+            ->left_join("reaction")
+            ->on(["reaction"=>"id","kinetics_law"=>"metabolic_node"])
+            ->where(["ec_number" => $ec])
+            ->select(["kinetics_law.*", "reaction.name"])
+            ;
+
         return [
-            "ec" => $ec
+            "ec" => $ec,
+            "law" => $laws
         ];
     }
 }
