ce2ae94906daed5e018147bccc34045a6d6a8292
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-22 00:14:36 +0800

    Update search.php

--- scripts/search.php
+++ scripts/search.php
@@ -4,12 +4,33 @@ class search {
 
     public function get_result($q) {
         $referer = $_SERVER['HTTP_REFERER'];
+        $referer = Utils::isDbNull($referer) ? null : URL::mb_parse_url ( $referer );
 
-        breakpoint($referer);
+        if (!Utils::isDbNull($referer)) {
+            return self::local_search($referer["path"], $q);
+        } else {
+            return self::global_search($q);
+        } 
+    }
 
+    private static function local_search($refer, $q) {
+        $encode_q = urlencode($q);
+
+        switch(strtolower($refer)) {
+            case "/compartments/":
+            case "/compartments":
+                Redirect("/compartments/?q={$encode_q}");
+                break;
+
+            default:
+                return self::global_search($q);
+        }
+    }
+
+    private static function global_search($q) {
         return [
             "title" => "Search Result of '{$q}'"
-        ];    
+        ];   
     }
 }
 
