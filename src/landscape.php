2b566e292d0142a18f35fc810ae0f3600686b884
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-02 18:02:16 +0800

    can not found the template page location?

---
+++ src/landscape.php
@@ -0,0 +1,14 @@
+<?php
+
+include __DIR__ . "/../.etc/bootstrap.php";
+
+class App {
+
+    /**
+     * @access *
+     * @uses view
+    */
+    public function metabolic_embedding() {
+        View::Display();
+    }
+}
