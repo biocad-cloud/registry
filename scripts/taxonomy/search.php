e1bd3dc47c111e09a3c9e83359b7bb7058a8a50f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-06 06:23:47 +0800

    show direct link

--- scripts/taxonomy/search.php
+++ scripts/taxonomy/search.php
@@ -3,6 +3,7 @@
 class search {
 
     public static function query_list($q, $page = 1) {
+        $name = $q;
         $term = Table::make_fulltext_strips($q);
         $page_size = 30;
         $list = (new Table(["cad_registry"=>"ncbi_taxonomy"]))
@@ -12,13 +13,30 @@ class search {
             ->limit(($page -1) * $page_size, $page_size)
             ->select(["ncbi_taxonomy.*","term as rank_name"])
             ;
+        $direct_name = (new Table(["cad_registry"=>"ncbi_taxonomy"]))
+            ->left_join("vocabulary")
+            ->on(["ncbi_taxonomy"=>"rank","vocabulary"=>"id"])
+            ->where(["name"=>$name])
+            ->find(["ncbi_taxonomy.*","term as rank_name"])
+            ;
 
         $page_num = $page;
         $data = [
             "taxon" => $list,
-            "q" => $term
+            "q" => $term,
+            "direct_link" => self::direct_link($direct_name)
         ];
 
         return list_nav( $data, $page_num);
+    }
+
+    private static function direct_link($tax) {
+        if (Utils::isDbNull($tax)) {
+            return "";
+        } else {
+            return "
+<strong>Taxonomy Find By Name:</strong> <a href='/taxonomy/?id={$tax["id"]}'>{$tax["name"]} ({$tax["rank_name"]})</a>
+            ";
+        }
     }
 }
