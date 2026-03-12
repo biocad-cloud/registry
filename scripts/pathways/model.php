197c7cf6969c9a5e56b33cf47320753867523cac
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-12 18:28:27 +0800

    get enzume lyst

--- scripts/pathways/model.php
+++ scripts/pathways/model.php
@@ -8,7 +8,13 @@ class pathway_model {
             ->left_join("metabolites")->on(["metabolites"=>"id", "pathway_network"=> "model_id"])
             ->left_join("struct_data")->on(["struct_data"=>"metabolite_id","metabolites"=>"id"])
             ->where(["class_id" => ENTITY_METABOLITE, "pathway_id" => $id, "metabolite_id" => not_null()])
-            ->select(["metabolite_id", "name", "formula", "exact_mass", "cas_id", "smiles"]);
+            ->select(["metabolite_id", "name", "formula", "exact_mass", "cas_id", "smiles"])
+            ;
+        $pwy["enzyme"] = (new Table(["cad_registry"=>"pathway_network"]))
+            ->left_join("enzyme")
+            ->on(["enzyme"=>"id","pathway_network"=>"model_id"])
+            ->where(["class_id"=> EC_NUMBER, "pathway_id"=> $id])
+            ->select(["`enzyme`.*", "symbol_id as ec_number"]);
 
         return $pwy;
     }
