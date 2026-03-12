edd0cee4ee15c80f6f34a31a12eee4a29ae78b5a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-12 20:52:55 +0800

    Create dbsearch.php

---
+++ scripts/dbsearch.php
@@ -0,0 +1,48 @@
+<?php
+
+class portal {
+
+    public static function db_search($q, $page=1, $page_size = 30) {
+        $q = Table::make_fulltext_strips($q);
+        $offset = ($page-1)*$page_size;
+        $sql = [
+            self::enzyme_search($q),
+            self::location_search($q)
+        ];
+        $sql = array_map(function($ql) {
+            return "({$ql})";
+        }, $sql);
+        $sql = Strings::Join($sql, " UNION ");
+        $sql = "SELECT * from ({$sql}) t1 ORDER BY score DESC LIMIT {$offset},{$page_size}";
+        $data = (new Table(["cad_registry"=>"vocabulary"]))->getDriver()->Fetch($sql);
+
+        return list_nav($data, $page);
+    }
+    
+    private static function enzyme_search($q) {
+        return "SELECT 
+        id,
+        recommended_name AS name,
+        note,
+        MATCH (recommended_name , systematic_name , note) AGAINST ('{$q}' IN BOOLEAN MODE) AS score,
+        'enzyme' AS type
+    FROM
+        cad_registry.enzyme
+    WHERE
+        MATCH (recommended_name , systematic_name , note) AGAINST ('{$q}' IN BOOLEAN MODE)";
+    }
+
+    private static function location_search($q) {
+        return "SELECT 
+        id,
+        name,
+        note,
+        MATCH (fullname , note) AGAINST ('{$q}' IN BOOLEAN MODE) AS score,
+        'location' AS type
+    FROM
+        cad_registry.compartment_location
+    WHERE
+        MATCH (fullname , note) AGAINST ('{$q}' IN BOOLEAN MODE)";
+    }
+}
+
