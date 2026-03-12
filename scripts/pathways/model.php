dc163c33c1ff9121a58fca4df27b4d67094a0e75
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-12 18:15:13 +0800

    show metabolites

--- scripts/pathways/model.php
+++ scripts/pathways/model.php
@@ -4,6 +4,11 @@ class pathway_model {
 
     public static function get_model($id) {
         $pwy = (new Table(["cad_registry"=>"pathway"]))->where(["id"=>$id])->find();
+        $pwy["metab"] = (new Table(["cad_registry"=>"pathway_network"]))
+            ->left_join("metabolites")->on(["metabolites"=>"id", "pathway_network"=> "model_id"])
+            ->left_join("struct_data")->on(["struct_data"=>"metabolite_id","metabolites"=>"id"])
+            ->where(["class_id" => ENTITY_METABOLITE, "pathway_id" => $id, "metabolite_id" => not_null()])
+            ->select(["metabolite_id", "name", "formula", "exact_mass", "cas_id", "smiles"]);
 
         return $pwy;
     }
