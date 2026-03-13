fa4e69bcc4b4a574c76931edf8517331ebcd5d9b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-13 18:52:06 +0800

    Create nav.enUS.php

---
+++ views/includes/nav.enUS.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "db" => "Database",
+    "browse" => "Browse",
+    "resource" => "Registry Resource",
+    "about" => "About",
+    "license" => "License",
+    "download" => "Download",
+    "search" => "Search"
+];
