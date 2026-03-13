2b91bdb6cff17446ffef8ec9759e786c5db2f58c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-13 18:53:45 +0800

    Create nav.zhCN.php

---
+++ views/includes/nav.zhCN.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "db" => "数据库搜索",
+    "browse" => "浏览",
+    "resource" => "注册表资源",
+    "about" => "关于",
+    "license" => "授权许可",
+    "download" => "下载",
+    "search" => "搜索"
+];
