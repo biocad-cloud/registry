8929d895dbd0c55106f4da6f97975d33a36afb5f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-20 11:48:19 +0800

    missing some data?

--- scripts/protein/cluster.php
+++ scripts/protein/cluster.php
@@ -6,9 +6,11 @@ class cluster_info {
         $data = (new Table(["cad_registry"=>"protein_data"]))
             ->left_join("ncbi_taxonomy")
             ->on(["ncbi_taxonomy"=>"id","protein_data"=>"ncbi_taxid"])
+            ->left_join("protein_cluster")
+            ->on("protein_cluster.query_id = {$id} AND protein_cluster.hit_id = protein_data.id")
             ->where(["cluster_id" => $id])
             ->limit(($page-1)* $page_size, $page_size)
-            ->select(["`protein_data`.id","source_id","`protein_data`.name","`function`","ncbi_taxid","ncbi_taxonomy.name as taxname"])
+            ->select(["`protein_data`.id","source_id","`protein_data`.name","`function`","ncbi_taxid","ncbi_taxonomy.name as taxname", "identities","mis_match","gap_open","e_value","bit_score"])
             ;
         $info = (new Table(["cad_registry"=>"protein_data"]))->where(["id"=>$id])->find();
         $info["prot"] = $data;
