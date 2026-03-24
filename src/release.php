b30aa5f68c01e0aa75b2e080b681b1f395823b2e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-24 20:48:24 +0800

    Create release.php

---
+++ src/release.php
@@ -0,0 +1,22 @@
+<?php
+
+<?php
+
+include __DIR__ . "/../.etc/bootstrap.php";
+
+class App {
+
+    /**
+     * @uses file
+    */
+    public function download_sophia($ver) {
+        $ver = trim($ver, ".");
+        $res_file = APP_PATH . "/release/sophia_{$ver}.msi";
+
+        if (!file_exists($res_file)) {
+            RFC7231Error::err404("Sorry, could not found the release file on server!");
+        } else {
+            Utils::PushDownload($res_file);
+        }
+    }
+}
